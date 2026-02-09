2e8f81eb61c1839e8f31dd5413d9be4387194614
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2026-02-09 17:30:04 +0000

    aula02

--- calculaMedia.js
+++ calculaMedia.js
@@ -1,17 +1,25 @@
-function gerarInteiro(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-
-let nota1 = gerarInteiro(1, 10);
-let nota2 = gerarInteiro(1, 10);
-let nota3 = gerarInteiro(1, 10);
+let nota1 = 6;
+let nota2 = 5;
+let nota3 = 4;
 
 const nomeAluno = "Bruno Wigers";
 
 let media = (nota1 + nota2 + nota3) / 3;
 
+
+if (media >= 7){
+  console.log("O aluno " + nomeAluno + " está aprovado.")
+}
+if(media >= 5 && media < 7 ){
+  console.log("O aluno " + nomeAluno + " está no SE LIGA. ")
+}
+{
+if (media < 5){
+  console.log("O aluno " + nomeAluno + " está reprovado.")
+}
+}
+
 console.log("O aluno: " + nomeAluno);
 console.log("Tem a média: " + media);
-
 
