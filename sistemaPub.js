6e108360e08b6c7a308c164405df53778656ed8e
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2026-03-04 13:05:38 +0000

    sistema pub pronto

---
+++ sistemaPub.js
@@ -0,0 +1,30 @@
+const readline = require('readline-sync');
+
+console.log("Sistema de acesso ao Pub do Wigers");
+
+const nome = readline.question("Qual seu nome? ");
+const idade = readline.questionInt("Qual sua idade? ");
+
+if (idade >= 18) {
+    console.log(`Olá ${nome}, sua entrada foi Autorizada. `);
+    const querBeber = readline.keyInYN("Deseja ver nosso cardápio de bebidas? ");
+    if (querBeber) {
+        console.log("Garçom a caminho! ")
+    } else {
+        console.log("Temos sucos também!");
+    }
+
+} else if (idade >= 16){
+    console.log(`Olá ${nome}, você precisa estar acompanhado...`)
+} else{
+    console.log(`Sinto muito ${nome}, apenas maiores que 16 podem beber!`)
+}
+
+
+
+
+
+
+
+
+
